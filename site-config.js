90bc347c74e347fe896dc5b42a230970030d0e0b
Tommy Ferdian Hadimarta <trickytommy4@gmail.com>
2020-09-23 11:17:24 +0700

    Update site config

--- site-config.js
+++ site-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "Tommy Ferdian Hadimarta",
   description: "Hi, I am Tommy Ferdian Hadimarta!",
-  url: "https://trickytommy.vercel.app",
+  url: "https://tommyf.now.sh",
   twitterUsername: "@TrickyTommy089",
   email: "trickytommy4@gmail.com",
 };
